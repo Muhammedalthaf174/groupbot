70d04210d7092db55bb3171f50671ea7ddb8efc6
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-03-01 09:31:46 +0530

    Update musichelp.py

--- Yuriko/modules/musichelp.py
+++ Yuriko/modules/musichelp.py
@@ -13,9 +13,5 @@ __help__ = """
 ❂ /skip - Play next song.
 ❂ /pause - Pause the currently playing song.
 ❂ /resume - Resume the paused song.
-❂ /msettings - To open panel settings on musicplayer.
-❂ /setadmin - To set all command only for admins.
-❂ /setmember - Set all command for all members.
-❂ /vol (range 1-200) - To set volume on voice calls.
 ❂ /end - stop the currently playing music.
 """
