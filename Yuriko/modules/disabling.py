9988b7a20489eb0224f629829514da6acb01f0f2
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-03-04 17:57:41 +0530

    Update disabling.py

--- Yuriko/modules/disabling.py
+++ Yuriko/modules/disabling.py
@@ -22,12 +22,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from Alakananda.modules.helper_funcs.chat_status import (
+    from Yuriko.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from Alakananda.modules.sql import disable_sql as sql
+    from Yuriko.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
